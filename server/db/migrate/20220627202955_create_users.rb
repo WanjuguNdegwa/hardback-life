3f08f28eca3decf7b998948881ef1e5fde0847ab
WanjuguNdegwa <ann.ndegwa@yahoo.com>
2022-06-28 00:38:01 +0300

    create users table

---
+++ server/db/migrate/20220627202955_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_hash
+      t.string :token
+      t.timestamps
+    end
+  end
+end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_190030) do
+ActiveRecord::Schema.define(version: 2022_06_27_202955) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2022_06_27_190030) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
