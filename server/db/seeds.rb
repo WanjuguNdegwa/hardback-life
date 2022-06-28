9b0a7d7b792fa98de4f1610c2a6b42d35fd5c1a6
WanjuguNdegwa <ann.ndegwa@yahoo.com>
2022-06-29 01:02:07 +0300

    improve seed data

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,28 +1,31 @@
-puts "🌱 Seeding books..."
+puts "🌱 Seeding..."
 
-# Make 50 games
+# Make 10 users
+10.times do
+  User.create(email: Faker::Internet.unique.email, password: Faker::Lorem.word)
+end
+
 20.times do
-  # create a game with random data
-  game = Book.create(
+  book = Book.create(
     title: Faker::Book.title,
     genre: Faker::Book.genre,
     author: Faker::Book.author,
+    description: Faker::Lorem.paragraph(random_sentences_to_add: 4)
   )
   
-  # # create between 1 and 5 reviews for each game
-  # rand(1..5).times do
-  #   # get a random user for every review
-  #   # https://stackoverflow.com/a/25577054
-  #   user = User.order('RANDOM()').first
+  # create between 1 and 5 reviews for each book
+  rand(1..5).times do
+    # get a random user for every review
+    # https://stackoverflow.com/a/25577054
+    user = User.order('RANDOM()').first
 
-  #   # A review belongs to a game and a user, so we must provide those foreign keys
-  #   Review.create(
-  #     score: rand(1..10),
-  #     comment: Faker::Lorem.sentence,
-  #     game_id: game.id,
-  #     user_id: user.id
-  #   )
-  # end
+    Review.create(
+      rating: rand(1..5),
+      comment: Faker::Lorem.sentence,
+      book_id: book.id,
+      user_id: user.id
+    )
+  end
 end
 
 puts "✅ Done seeding!"
