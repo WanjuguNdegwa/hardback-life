87f575973360a39032022474f80c399deefe2104
WanjuguNdegwa <ann.ndegwa@yahoo.com>
2022-07-01 07:13:43 +0300

    Add review component

---
+++ client/src/components/Review.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {default as Rating} from "react-rating-stars-component";
+
+import './Review.css';
+
+function Review({review}) {
+  return (
+    <div className='review my-3'>
+      <div className="d-flex justify-content-between">
+        <p className='reviewer'>{review.user.email}</p>
+        <Rating
+          count={5}
+          size={12}
+          edit={false}
+          value={review.rating}
+          activeColor="#ffd700"
+        />
+      </div>
+      {/* <blockquote class="blockquote"> */}
+        <p className='comment'>{review.comment}</p>
+      {/* </blockquote> */}
+    </div>
+  )
+}
+
+export default Review
